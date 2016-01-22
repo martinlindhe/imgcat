3418d756083256fed5873129e882a94b2056f710
Martin Lindhe <martin@ubique.se>
2016-01-22 11:17:30 +0100

    use io.Reader, io.Writer

--- lib/imgcat.go
+++ lib/imgcat.go
@@ -7,69 +7,72 @@ import (
 	"image"
 	"image/png"
 	"io"
-	"io/ioutil"
 	"os"
 )
 
 // CatRGBA embeds given image.RGBA in the terminal output (iTerm 2.9+)
-func CatRGBA(i *image.RGBA) {
+func CatRGBA(i *image.RGBA) error {
 
-	data := imageAsPngBytes(i)
-
-	embed(data, os.Stdout)
+	return embed(imageAsPngBytes(i), os.Stdout)
 }
 
 // CatImage embeds given image.Image in the terminal output (iTerm 2.9+)
-func CatImage(i *image.Image) {
+func CatImage(i *image.Image) error {
 
-	data := imageAsPngBytes(*i)
-
-	embed(data, os.Stdout)
+	return embed(imageAsPngBytes(*i), os.Stdout)
 }
 
 // CatFile embeds given image file in the terminal output (iTerm 2.9+)
 func CatFile(fileName string) error {
 
-	raw, err := ioutil.ReadFile(fileName)
+	r, err := os.Open(fileName)
 	if err != nil {
 		return err
 	}
 
-	embed(raw, os.Stdout)
+	embed(r, os.Stdout)
 	return nil
 }
 
 // CatReader embeds given io.Reader in the given io.Writer
-func CatReader(r io.Reader, w io.Writer) {
+func CatReader(r io.Reader, w io.Writer) error {
+
+	return embed(r, w)
+}
+
+func embed(r io.Reader, w io.Writer) error {
 
 	buf := new(bytes.Buffer)
 	buf.ReadFrom(r)
-	embed(buf.Bytes(), w)
-}
 
-func embed(data []byte, w io.Writer) {
-	enc := base64.StdEncoding.EncodeToString(data)
+	fmt.Printf("wrote %d bytes\n", len(buf.Bytes()))
 
 	// tmux requires unrecognized OSC sequences to be wrapped with DCS tmux;
 	// <sequence> ST, and for all ESCs in <sequence> to be replaced with ESC ESC. It
 	// only accepts ESC backslash for ST.
 	fmt.Fprint(w, "\033Ptmux;\033\033]")
 
-	fmt.Fprint(w, "1337;File=")
-	fmt.Fprint(w, ";inline=1")
-	fmt.Fprint(w, ":")
-	fmt.Fprint(w, enc)
+	fmt.Fprintf(w, "1337;File=;inline=1:")
+
+	encoder := base64.NewEncoder(base64.StdEncoding, w)
+	_, err := encoder.Write(buf.Bytes())
+	if err != nil {
+		return err
+	}
+	encoder.Close()
 
 	// More of the tmux workaround described above.
-	fmt.Println(w, "\a\033\\")
+	fmt.Fprintln(w, "\a\033\\")
+
+	return nil
 }
 
-func imageAsPngBytes(i image.Image) []byte {
+func imageAsPngBytes(i image.Image) io.Reader {
 
 	buf := new(bytes.Buffer)
 	err := png.Encode(buf, i)
 	if err != nil {
 		fmt.Println(err)
 	}
-	return buf.Bytes()
+	return buf
 }
